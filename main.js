347c88d53659371b32df74a8ce1799fd27171c98
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-10-04 19:42:55 +0300

    select options works

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+const breed = document.getElementById("breed");
+
+// main function - allows us to get the data
 async function start() {
   const response = await fetch("https://dog.ceo/api/breeds/list/all");
   const data = await response.json();
-  console.log(data);
+
+  createBreedList(data.message);
 }
+start();
+
+// function to get options of breeds
+const createBreedList = (information) => {
+  breed.innerHTML = `<select>
+    <option>Choose your breed</option>
+    ${Object.keys(information).map((info) => {
+      return `<option>${info}</option>`;
+    })}
+  </select>`;
+};
 
 start();
